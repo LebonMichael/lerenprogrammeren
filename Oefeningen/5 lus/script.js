062080a195384b57fb575e3c856beec1a6288424
Lebon Michael <lebontje45@hotmail.com>
2021-09-15 12:34:16 +0200

    oefening 5

--- Oefeningen/5 lus/script.js
+++ Oefeningen/5 lus/script.js
@@ -7,6 +7,7 @@
 // 2 De totale som van 3 getallen is 9.
 
 
+/*
 var aantal; // declareren variabele
 var teller = 1;
 var getal;
@@ -18,4 +19,14 @@ for ( teller;teller<=aantal;teller++){
     getal = parseInt(prompt("Geef getal "+ teller + " in "));
     resultaat = resultaat + getal;
 }
-document.getElementById("oef5").innerText = "De totale some van "+ aantal + " getallen is " + resultaat;
+document.getElementById("oef5").innerText = "De totale some van "+ aantal + " getallen is " + resultaat;*/
+
+var aantal
+var teller = 1
+var sterren = ""
+
+aantal = parseInt(prompt("Geef aantal sterren"));
+for(teller;teller<=aantal;teller++){
+    sterren = sterren + "*";
+    console.log(sterren)
+    }
