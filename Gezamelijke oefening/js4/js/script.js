18641428e6f4f07cb43d417847be625ad46ed532
Lebon Michael <lebontje45@hotmail.com>
2021-09-15 09:32:10 +0200

    meervoudige selectie

---
+++ Gezamelijke oefening/js4/js/script.js
@@ -0,0 +1,28 @@
+//Meervoudige selectie
+
+/*
+var onderwijs = prompt("Geef onderwijs in");
+
+switch (onderwijs){
+    case "vdab":
+        console.log("cursus is gegeven door VDAB");
+        break;
+    case "syntra":
+        console.log("cursus is gegeven door Syntra");
+        break;
+    default:
+        console.log("gegeven door een andere instelliing");
+}*/
+
+//enkelvoudige selectie shorthand
+
+/*
+var x = 1;
+if(x == 1){
+    //code
+}else{
+    //code
+}
+
+x==1 ? // code : // code
+*/
