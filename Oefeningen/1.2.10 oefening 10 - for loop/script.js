f3f0a850e5405c2741f66c9f58222614cd15521d
Lebon Michael <lebontje45@hotmail.com>
2021-09-17 01:22:43 +0200

    oefeningen

---
+++ Oefeningen/1.2.10 oefening 10 - for loop/script.js
@@ -0,0 +1,38 @@
+/*
+
+var tafel = parseInt(prompt("Geef tafel"));
+var Einde = parseInt(prompt("Geef eind getal"));
+var i = 1;
+var som = 0;
+var resultaat = "";
+
+for(i;i<=Einde;i++){
+    som = tafel * i;
+    resultaat = resultaat + tafel + "+" + i + "=" + som + ",";
+}
+console.log(resultaat.substr(0,resultaat.length-1));
+
+*/
+
+
+
+/*
+
+var tafel = parseInt(prompt("Geef tafel"));
+var einde = parseInt(prompt("Geef eind getal"));
+var i = 1;
+var som = 0;
+var resultaat = "";
+
+for(i;i<=einde;i++){
+    som = tafel * i;
+    if(som<=einde){
+        resultaat = resultaat + tafel + "+" + i + "=" + som + ",";
+    }
+    else{
+      break;
+    }
+}
+console.log(resultaat.substr(0,resultaat.length-1));
+
+*/
---
+++ Oefeningen/1.2.9 oefening 9 - for loop/script.js
@@ -0,0 +1,12 @@
+var getal = parseInt(prompt("Geef eind getal in"))
+var i = 2
+
+
+for(i;i<=getal;i++){
+    if(i/2 === 1 ){
+        console.log(i)
+    }else{
+
+    }
+
+}
