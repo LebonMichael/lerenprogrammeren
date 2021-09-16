11686317765be8b4cfbed82f10a51cd89273aeef
Lebon Michael <lebontje45@hotmail.com>
2021-09-16 22:55:04 +0200

    oefeningen

--- Oefeningen/1.2.8 oefening 8 - for loop/script.js
+++ Oefeningen/1.2.8 oefening 8 - for loop/script.js
@@ -1,6 +1,8 @@
 var getal = parseInt(prompt("Geef getal"));
-var i = 1
+var i = 1;
+var som = "";
 
 for(i;i<=getal;i*=2){
-    console.log(i);
+    som = som + i + ",";
 }
+console.log(som.substr(0,som.length));
