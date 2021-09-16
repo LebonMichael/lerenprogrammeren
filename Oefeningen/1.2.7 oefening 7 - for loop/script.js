d423b7869e99cd2d67ad4a1966dd245ac0118caf
Lebon Michael <lebontje45@hotmail.com>
2021-09-16 22:41:31 +0200

    oefeningen

--- Oefeningen/1.2.7 oefening 7 - for loop/script.js
+++ Oefeningen/1.2.7 oefening 7 - for loop/script.js
@@ -1,22 +1,3 @@
-/*
-let text = prompt("Geef tekst")
-let karakter = prompt("Geeft karakter")
-let i = 1
-let aantal = 0
-let o = 0
-
-for(i;i<=text.length;i++){
-
-    if(karakter===text.substring(o,i)){
-        aantal++
-        o++
-    }else{
-        o++
-    }
-}
-console.log(aantal);
-*/
-
 var gevraagdGetal = parseInt(prompt("Geef een getal in"));
 var i = 1
 
---
+++ Oefeningen/1.2.8 oefening 8 - for loop/script.js
@@ -0,0 +1,6 @@
+var getal = parseInt(prompt("Geef getal"));
+var i = 1
+
+for(i;i<=getal;i*=2){
+    console.log(i);
+}
