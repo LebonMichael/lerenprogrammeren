bc5fe68ffcb6c58027cefe24a7bdd06f3dc25e1c
Lebon Michael <lebontje45@hotmail.com>
2021-09-17 14:32:58 +0200

    oefeningen

--- Oefeningen/1.2.11_oefening_11_-_for_loop/script.js
+++ Oefeningen/1.2.11_oefening_11_-_for_loop/script.js
@@ -2,17 +2,18 @@ var getal1 = 0
 var getal2 = 1
 var som = 0
 var aantalGetallen = parseInt(prompt("Geef in hoeveel getallen"));
-var i = 2
+var i = 1
 var resultaat = ""
 
 for(i;i<=aantalGetallen;i++){
-    if(i%2===1){
-        som = som + getal1;
-        getal1 = som;
-    }else
+    if(i%2 === 0){
         som = som + getal2;
         getal2 = som;
-    resultaat = resultaat + som + "+";
+    }else{
+        som = som + getal1;
+        getal1 = som;
+    }
+    resultaat = resultaat + som + ",";
 
 }
 console.log(resultaat.substr(0,resultaat.length-1));
