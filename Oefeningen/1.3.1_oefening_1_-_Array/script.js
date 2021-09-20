2861d79e481beb759e26f05aed26f53a5892c3a0
Lebon Michael <lebontje45@hotmail.com>
2021-09-20 11:38:08 +0200

    oefeningen

---
+++ Oefeningen/1.3.1_oefening_1_-_Array/script.js
@@ -0,0 +1,12 @@
+var getal = 1
+var i = 0
+var resultaat = []
+for(i;getal>=0;i++) {
+    getal = (parseInt(prompt("Geef getal in")));
+    if(getal>=0){
+        resultaat.push(getal)
+    }
+    console.log(resultaat);
+}
+var welkGetal = parseInt(prompt("Welke getal wil je"))
+console.log(resultaat[welkGetal])
