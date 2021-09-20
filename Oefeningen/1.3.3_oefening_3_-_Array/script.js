a11ec4bb4860693148c3ce1b43936f3925ae0ca9
Lebon Michael <lebontje45@hotmail.com>
2021-09-20 13:28:13 +0200

    oefeningen

--- Oefeningen/1.3.3_oefening_3_-_Array/script.js
+++ Oefeningen/1.3.3_oefening_3_-_Array/script.js
@@ -1,10 +1,14 @@
 var array1 = [];
 var array2 = [];
 var array3 = [];
+function randomGetal(max) {
+    return Math.floor(Math.random() * max);
+}
+
 
 for(var i = 1;i<=10; i++){
-    array1.push(i)
-    array2.push(i)
+    array1.push(randomGetal(100))
+    array2.push(randomGetal(100))
     var resultaat = array1[i-1] + array2[i-1]
     array3.push(resultaat)
     console.log(array1[i-1] + "+" + array2[i-1] + "=" + array3[i-1])
---
+++ Oefeningen/1.3.4_oefening_4_-_Array/script.js
@@ -0,0 +1,10 @@
+var aantalNamen = parseInt(prompt("Hoeveel namen?"));
+var i = 1
+var klassenLijst = []
+
+
+for(i;i<=aantalNamen;i++){
+    var naam = prompt("Geef naam")
+        klassenLijst.push("- " + naam)
+    console.log(klassenLijst[i-1])
+}
